cba67b730e0ed0b7a0cc709be489cf5c1f2eedea
DhruvParmar051 <dhruvparmar051@outlook.com>
2025-08-08 18:52:46 +0530

    assignmen 1 completed

--- assignment/preproceesing/preprocessing.py
+++ assignment/preproceesing/preprocessing.py
@@ -3,12 +3,11 @@
 import pandas as pd
 
 
-class preprocessor:
+class Preprocessor:
     def __init__(self, path):
         self.book_df = pd.read_csv(path)
         
     def convert_ratings(self):
-        """convert string star rating to numbers """
 
         rating_map = {
             'One':1,
@@ -22,29 +21,29 @@ class preprocessor:
         
     
     def clean_prices(self):
-        """Remove currency symbol (£) and convert price to float."""
-        self.book_df['book_price'] = self.book_df['book_price'].replace(
-            to_replace='£', value='', regex=True
-        ).astype(float)  
-        
+        self.book_df['book_price'] = self.book_df['book_price'].astype(str).str.replace('£', '', regex=False).astype(float)
+
+
     def clean_stock(self):
         self.book_df['book_instock'] = self.book_df['book_instock'].apply(
-            lambda x: 'In stock' in x if isinstance(x, str) else False
+        lambda x: 'In stock' in str(x)
         )
+
         
     def save_to_csv(self,out_path='books.csv'):
         self.book_df.to_csv(out_path, index=False)
         
-        
-path = r'D:\DAU\SEM 1\DS605 - Fundamentals of Machine Learning\Lab\Lab02DhruvParmar051\assignment\assignment\books.csv'
 
-books_df = preprocessor(path)
-print('book df successful loaded')
-books_df.clean_prices()
-print('book_df price attribute cleaned')
-books_df.clean_stock()
-print('book_df in stock attribute cleaned')
-books_df.convert_ratings()
-print('book_df string ratings converted to numeric')
-books_df.save_to_csv(path)
-print('book_df is now saved as CSV named books.csv')
+if __name__ == '__main__':
+    path = r'D:\DAU\SEM 1\DS605 - Fundamentals of Machine Learning\Lab\Lab02DhruvParmar051\assignment\assignment\spiders\books.csv'
+
+    books_df = Preprocessor(path)
+    print('book df successfully loaded')
+    books_df.clean_prices()
+    print('book_df price attribute cleaned')
+    books_df.clean_stock()
+    print('book_df in stock attribute cleaned')
+    books_df.convert_ratings()
+    print('book_df string ratings converted to numeric')
+    books_df.save_to_csv(path)
+    print('book_df is now saved as CSV named books.csv')
